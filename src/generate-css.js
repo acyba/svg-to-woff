eb819dabcf66fb7f99edee68e9100a564196830e
Jérémy Dziergwa <jeremy.dziergwa@jetpulp.fr>
2025-03-04 09:14:08 +0100

    0.0.8 - prevent cache on font files

--- src/generate-css.js
+++ src/generate-css.js
@@ -7,14 +7,16 @@ export const generateCSS = async(metadata, fontName, outputFolder) => {
     const outputCSSFile = `${outputFolder}/${fontName}.css`;
     const outputFontFileWoffUrl = `${fontName}.woff`;
     const outputFontFileWoff2Url = `${fontName}.woff2`;
+    const randomString = Math.random().toString(36).substring(7);
 
     try {
         let cssContent = `
 /* Icons font: ${fontName} */
 @font-face {
     font-family: '${fontName}';
-    src: url('${outputFontFileWoff2Url}') format('woff2'), url('${outputFontFileWoffUrl}') format('woff');
+    src: url('${outputFontFileWoff2Url}?${randomString}') format('woff2'), url('${outputFontFileWoffUrl}?${randomString}') format('woff');
 }
+
 #acym_wrapper [class^="${classPrefix}"], #acym_wrapper [class*=" ${classPrefix}"], [class^="${classPrefix}"], [class*=" ${classPrefix}"]  {
     font-family: '${fontName}';
     font-style: normal;
